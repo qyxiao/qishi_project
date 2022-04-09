92e814160f07aa0c07a68da264ead002cca087c5
qyxiao <qx344@nyu.edu>
2022-04-09 18:02:48 -0400

    publisher and orderbook should change

--- Project1/Project2/main.cpp
+++ Project1/Project2/main.cpp
@@ -1,28 +1,41 @@
-//#include "Price4.h"
 #include <iostream>
 #include <json.hpp>
 #include "Publisher.h"
+#include <fstream>
+#include "Loader.h"
+#include "MatchEngine.h"
+#include "Publisher.h"
 
 using json = nlohmann::json;
+using namespace std;
 
 int main() {
 
-	json j = {
-  {"pi", 3.141},
-  {"happy", true},
-  {"name", "Niels"},
-  {"nothing", nullptr},
-  {"answer", {
-	{"everything", 42}
-  }},
-  {"list", {1, 0, 2}},
-  {"object", {
-	{"currency", "USD"},
-	{"value", 42.99}
-  }}
-	};
+	Publisher pb = Publisher("pretty.json");
+	vector<string> msg = { "1", "2", "3", "4", "5" };
+	pb.deliver_msg(msg);
 
-	std::cout << j.dump(4) << std::endl;
+	//json j = {
+ // {"pi", 3.141},
+ // {"happy", true},
+ // {"name", "Niels"},
+ // {"nothing", nullptr},
+ // {"answer", {
+	//{"everything", 42}
+ // }},
+ // {"list", {1, 0, 2}},
+ // {"object", {
+	//{"currency", "USD"},
+	//{"value", 42.99}
+ // }}
+	//};
+
+	//ofstream outs("pretty.json");
+
+	//outs << j << std::endl;
+
+	//ofstream outs2("pretty.json", fstream::app);
+	//outs2 << j << std::endl;
     
 	return 0;
 }
